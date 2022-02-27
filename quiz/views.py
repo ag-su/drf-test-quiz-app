1a16c2e67fe2c484fd60dd8ed03170484001392c
sookim06 <eunsugim815@gmail.com>
2022-02-27 15:59:20 +0900

    Ready for deploy

---
+++ quiz/views.py
@@ -0,0 +1,14 @@
+from .serializers import QuizSerializer
+import random
+
+# Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("hello world!")
+
+@api_view(['GET'])
+def randomQuiz(request, id): 
+    totalQuizs = Quiz.objects.all()
+    randomQuizs = random.sample(list(totalQuizs), id)
+    serializer = QuizSerializer(randomQuizs, many=True)
+    return Response(serializer.data)
